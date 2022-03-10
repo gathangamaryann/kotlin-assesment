7c8dde08df21c31b14396b9191a0283468db4b36
gathangamaryann <maryanngathanga@gmail.com>
2022-03-10 08:58:05 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main (){
+ //print out a String composed of the fifth,sixth,seventh and eight character
+  //girls
+}
+fun myschool () {
+
+
+var school = ("codeHive")
+println([6])
+    println([7])
+    println([8])
+println([9])
+}
+fun girls(){
+    var numbers =  ( 5: 11)
+    println numbers( 5$ 11)
+
+}
+fun myidentity(){
+    var age =21
+     var name =("maryann")
+    println (hi my name is $ and am $ years old)
+}
+fun girls(){
+    var arrayof ("mary","jume","joy")
+    println girls(String.length)
+}
+
+
