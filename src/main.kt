1a32deee7a1522532d015825a73c57c1d8895321
gathangamaryann <maryanngathanga@gmail.com>
2022-03-10 10:34:59 +0300

    modify main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,39 @@
 fun main (){
  //print out a String composed of the fifth,sixth,seventh and eight character
-  //girls
+    coding()
+    modulus()
+    myidentity()
+    girls()
 }
-fun myschool () {
+fun coding () {
+var school = "codeHive"
 
-
-var school = ("codeHive")
-println([6])
-    println([7])
-    println([8])
-println([9])
+//    var char 5 = (school[6])
+//    var char 6 = (school[7])
+//    var char 7 = (school[8])
+//    var char 8 = (school[9])
+//    var result = (char5char7char8$char9")
+    var characters = school.get(4)+school.get(5).toString()+school.get(6)+school.get(7)
+println(characters)
 }
-fun girls(){
-    var numbers =  ( 5: 11)
-    println numbers( 5$ 11)
-
+fun modulus(){
+    var  a = 11
+    var b = 2
+    var answer = a%b
+    println(answer)
+//
 }
 fun myidentity(){
-    var age =21
-     var name =("maryann")
-    println (hi my name is $ and am $ years old)
+var z = 21
+    var p = "MaryAnne"
+    println ("Hi my name is $p and am $z years old")
 }
 fun girls(){
-    var arrayof ("mary","jume","joy")
-    println girls(String.length)
+
+
+    var cap = "MaryAnne"
+println(cap.length)
+
 }
 
 
